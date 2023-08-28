913defe4527edf45a8482474fe2a22cc0c7d1044
Wilmer R <wilmer.rodriguez-r@mail.escuelaing.edu.co>
2023-08-27 21:02:51 -0500

    Swagger por fin

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import jdk.jfr.Enabled;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class DemoApplication {
--- src/main/java/com/example/demo/controller/health/HealthController.java
+++ src/main/java/com/example/demo/controller/health/HealthController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller.health;
 
+import io.swagger.annotations.Api;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api
 @RestController
 public class HealthController {
 
